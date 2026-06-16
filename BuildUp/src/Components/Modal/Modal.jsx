2926fde7591a10c5d81569cf1bf9603163745bb4
Júlia Alves <jju.allvs@gmail.com>
2026-06-16 13:58:37 -0300

    mudanças simulador

--- BuildUp/src/Components/Modal/Modal.jsx
+++ BuildUp/src/Components/Modal/Modal.jsx
@@ -1,19 +1,45 @@
 import "./modal.css";
 
-export default function Modal({ aberto, titulo, mensagem, onFechar }) {
-  if (!aberto) return null;
+export default function Modal({
+    aberto,
+    titulo,
+    mensagem,
+    onFechar,
+    onConfirmar,
+    tipo = "info" // info | confirm
+}) {
+    if (!aberto) return null;
 
-  return (
-    <div className="modal-overlay" onClick={onFechar}>
-      <div className="modal" onClick={(e) => e.stopPropagation()}>
-        <h2>{titulo}</h2>
+    return (
+        <div className="modal-overlay" onClick={onFechar}>
+            <div className="modal" onClick={(e) => e.stopPropagation()}>
+                <h2>{titulo}</h2>
+                <p>{mensagem}</p>
 
-        <p>{mensagem}</p>
+                <div className="modal-botoes">
+                    {tipo === "confirm" ? (
+                        <>
+                            <button
+                                className="btn-modal cancelar"
+                                onClick={onFechar}
+                            >
+                                Cancelar
+                            </button>
 
-        <button className="btn-modal" onClick={onFechar}>
-          Ok
-        </button>
-      </div>
-    </div>
-  );
+                            <button
+                                className="btn-modal confirmar"
+                                onClick={onConfirmar}
+                            >
+                                Confirmar
+                            </button>
+                        </>
+                    ) : (
+                        <button className="btn-modal" onClick={onFechar}>
+                            Ok
+                        </button>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
 }
